ae5567c170044a6b8c253ccdd36988fe099874b1
Gage Bovi <gage.bovi@gmail.com>
2025-01-25 11:49:42 -0600

    lala boo boo boo boo boo

--- bubble/area_2d.gd
+++ bubble/area_2d.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends CharacterBody2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -7,9 +7,14 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 
-func _on_body_entered(body: Node2D) -> void:
-	print("ENTERD")
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("lalala")
+
+func _physics_process(delta: float) -> void:
+	velocity.x = 1
+	velocity.y = 0
+	move_and_slide()
+	
