25a854b6016ef4bc9699ee40ae1e2b647258eabd
Judah <judah.ingles@outlook.com>
2025-01-26 14:42:15 -0600

    fixed collisions

--- bubble/main.gd
+++ bubble/main.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var bubble = preload("res://bubble.tscn")
 var bubble_spawner
+@onready var BubbleSound = $bubbleboy/BubbleCreate
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,5 +16,6 @@ func _process(delta: float) -> void:
 			bubble_spawner.queue_free()
 		bubble_spawner = bubble.instantiate()
 		add_child(bubble_spawner)
+		BubbleSound.play
 		bubble_spawner.position.x = $bubbleboy.position.x + 32
 		bubble_spawner.position.y = $bubbleboy.position.y + 16
